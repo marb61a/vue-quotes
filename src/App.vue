a3a2e9e86679820f6d7c4d8bcdf7a65ccb13dd60
marb61a <marb61a@gmail.com>
2018-05-11 17:16:31 +0100

    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="container">
+    <app-header :quote-count="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
     <div class="row">
-      <div class="alert alert-info">
-        Info: Click on a Quote to delete it!
-      </div>
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: Click on a Quote to delete it!
+        </div>  
+      </div>  
     </div>
   </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
 import Header from './components/Header.vue';
 
 export default {
@@ -27,21 +34,18 @@ export default {
         return alert('Please delete Quotes first!');
       }
       this.quotes.push(quote);
+    },
+    deleteQuote(quote){
+      this.quotes.splice(index, 1);
     }
   },
   components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
     appHeader: Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
